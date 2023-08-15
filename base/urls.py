f9d68afced5fdaa09562aa2da88880991e29cf66
obafemi-i <obaff76@gmail.com>
2023-08-15 08:57:53 +0100

    initial commit

---
+++ base/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+
+    path('login/',views.loginPage, name='login'),
+    path('logout/', views.logoutPage, name='logout'),
+
+    path('register/', views.register, name='register'),
+    path('user-profile/<str:pk>/', views.profile, name='user-profile'),
+
+    path('room/<str:pk>/', views.room, name='room'),
+    path('create-room/', views.createRoom, name='create-room'),
+    path('update-room/<str:pk>/', views.updateRoom, name='update-room'),
+    path('delete-room/<str:pk>/', views.deleteRoom, name='delete-room'),
+
+    path('delete-message/<str:pk>/', views.deleteMessage, name='delete-message')
+
+]
